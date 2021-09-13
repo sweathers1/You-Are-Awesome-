d6dcf191f74bc77ac50356ed164769497f14dce8
Sydney Weathers <sweathers@zagmail.gonzaga.edu>
2021-09-13 15:57:36 -0700

    Completed Ch 2.11

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     
     override func viewDidLoad() {
@@ -24,21 +25,25 @@ class ViewController: UIViewController {
         let messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fantastic!",
-                        "Fabulous? That's You!"]
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of Jony Ive"]
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
+//        }
         
 //        let imageName = "image" + String(imageNumber)
-        let imageName = "image\(imageNumber)"
-        imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
+//        let imageName = "image\(imageNumber)"
+//        imageView.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
+//        }
         
         
 //        let awesomeMessage = "You Are Awesome!"
